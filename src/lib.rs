0856e5873c63c40503b438ca76f23e01d3569626
Jay Logelin <lodge@protocol.ai>
2022-08-14 23:57:18 -0300

    complete refactor of project structure, use of wasm-bindgen-rayon for wasm multi-threading, initial state preservation implemented

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,16 @@ use lurk::{
     store::{ContTag, Pointer, Store},
     writer::Write,
 };
+use anyhow::Result;
 use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 use serde_json::json;
 use blstrs::Scalar as Fr;
 
 use std::panic;
+use web_sys::console;
+
+pub use wasm_bindgen_rayon::init_thread_pool;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -18,94 +22,83 @@ use std::panic;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-/* 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
-}
-*/
-
-//#[wasm_bindgen]
-//pub fn initi() { utils::set_panic_hook() }
-
 #[wasm_bindgen]
 pub fn init_panic_hook() {
     utils::set_panic_hook();
 }
 
-/// Run a lurk snippet
-#[wasm_bindgen(catch)]
-pub fn execute_lurk(source: JsValue) -> Result<JsValue, JsValue> {
-    let expression = source
-        .as_string()
-        .ok_or_else(|| "input source must be a string")?;
+#[wasm_bindgen(module = "/js/panic-handler.js")]
+extern "C" {
+    fn onPanic(message: String);
+}
 
-    let result = panic::catch_unwind(|| {
-        run_lurk(expression)
-    });
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
 
-    if result.is_err() {
+    panic::set_hook(Box::new(|panic_info| {
+        let msg = format!("{}", panic_info);
+        console::log_1(&JsValue::from_str(&msg));
+        onPanic(msg);
+    }));    
+
+    // rayon::ThreadPoolBuilder::new().num_threads(1).build_global().unwrap();
+
+    // Your code goes here!
+    console::log_1(&JsValue::from_str("Hello world!"));
+
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub struct Repl {
+    store: Store<Fr>,
+}
+
+#[wasm_bindgen]
+impl Repl {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Repl {
+        Repl { store: Store::<Fr>::default() }
+    }
+
+    /// Run a lurk snippet
+    #[wasm_bindgen]
+    pub fn execute_lurk(&mut self, source: JsValue) -> Result<JsValue, JsValue> {
+        let limit = 100_000_000;
+
+        let expression = source
+            .as_string()
+            .ok_or_else(|| "input source must be a string")?;
+
+        //let mut store = Store::<Fr>::default();
         let mut context: HashMap<&str, String> = HashMap::new();
-        let s = format!("{:#?}", result.err());
-        context.insert("errors", s );
+
+        context.insert("expression", expression.clone());
+        if let Some(expr) = self.store.read(&expression) {
+            let (output, iterations, _) =
+                Evaluator::new(expr, empty_sym_env(&self.store), &mut self.store, limit).eval();
+
+            let iterations_str = iterations.to_string();
+            context.insert("iterations", iterations_str);
+            let result_str = match output.cont.tag() {
+                ContTag::Outermost | ContTag::Terminal => {
+                    let result = self.store.fetch(&output.expr).clone().unwrap();
+                    result.fmt_to_string(&self.store)
+                }
+                ContTag::Error => "ERROR!".to_string(),
+                _ => format!("Computation incomplete after limit: {}", limit),
+            };
+
+            context.insert("result", result_str);
+        } else {
+            let error = format!("Syntax Error: {}", &expression);
+            context.insert("result", error);
+        }
         let json = json!(&context);
         Ok(json.to_string().into())
-    } else {
-        //let context = run_lurk(expression);
-        let context = result.unwrap();
-        let json = json!(&context);
-        Ok(json.to_string().into())
-    }
-}
-
-fn run_lurk(expression: String) -> HashMap<&'static str, String> {
-    let limit = 100_000_000;
-    let mut store = Store::<Fr>::default();
-    let mut context: HashMap<&str, String> = HashMap::new();
-
-    context.insert("expression", expression.clone());
-
-    if let Some(expr) = store.read(&expression) {
-        let (output, iterations, _ptrs) =
-            Evaluator::new(expr, empty_sym_env(&store), &mut store, limit).eval();
-
-        let iterations_str = iterations.to_string();
-        context.insert("iterations", iterations_str);
-        let result_str = match output.cont.tag() {
-            ContTag::Outermost | ContTag::Terminal => {
-                let result = store.fetch(&output.expr).clone().unwrap();
-                result.fmt_to_string(&store)
-            }
-            ContTag::Error => "ERROR!".to_string(),
-            _ => format!("Computation incomplete after limit: {}", limit),
-        };
-
-        context.insert("result", result_str);
-    } else {
-        let error = format!("Syntax Error: {}", &expression);
-        context.insert("result", error);
-    }
-    println!("{:?}", store);
-    return context;
-}
-
-#[cfg(test)]
-mod tests {
-    use wasm_bindgen::JsValue;
-
-    use crate::{execute_lurk, run_lurk};
-
-    #[test]
-    fn test_execute_lurk() {
-        // let result = execute_lurk(JsValue::from_str("(* 8 7)"));
-        let result = run_lurk(String::from("(* 8 7)"));
-        println!("{:?}", result);
-        // assert_eq!(result, 4);
-        assert_eq!(4, 4);
     }
 }
