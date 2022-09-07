0e951607211359a7ba9fe2575ca24a93c14f8000
Jay Logelin <lodge@protocol.ai>
2022-09-07 15:43:46 -0300

    disable/hide lurk controls while worker threads are loading in the browser

--- js/index.js
+++ js/index.js
@@ -4,18 +4,20 @@ import * as Comlink from 'comlink';
 var originalContents;
 
 (async function init() {
-
     originalContents = document.getElementById("lurkcode").textContent;
     HighlightLisp.highlight_auto();
     HighlightLisp.paren_match();
     
-
     // Create a separate thread from wasm-worker.js and get a proxy to its handlers.
     let handlers = await Comlink.wrap(
       new Worker(new URL('./wasm-worker.js', import.meta.url), {
         type: 'module'
       })
     ).handlers;
+
+    console.log('Worker Threads LOADED');
+    var output_container = document.getElementById("lurk-code-component-controls");
+    output_container.style.display = "block";
 
     // let handler = handlers['singleThread'];
     let handler = handlers['multiThread'];
@@ -53,7 +55,6 @@ var originalContents;
     }
 
     document.addEventListener("update-progress", (event) => {
-        console.log("WE GOT ONE!!!!!!!!!");
         var output = document.getElementById("output");
         output.textContent = event.detail.message();
     });
@@ -65,40 +66,3 @@ var originalContents;
     }
     */
 })();
-
-/*
-import("../pkg/index.js").then(async module => {
-    await init();
-    await module.initThreadPool(navigator.hardwareConcurrency);
-    //init_panic_hook();
-    HighlightLisp.highlight_auto();
-    HighlightLisp.paren_match();
-    originalContents = document.getElementById("lurkcode").textContent;
-
-    var btn = document.getElementById('run');
-    btn.onclick = function (e) {
-        var output_container = document.getElementById("output-container");
-        output_container.style.display = "block";
-        var lurkcode = document.getElementById("lurkcode");        
-        var output = document.getElementById("output");
-        try {
-            var out = module.execute_lurk(lurkcode.textContent);
-        } catch (error) {
-            console.log(error);
-            output.textContent ="Iterations: 0 \nResult: ERROR: " + error;
-            return false;
-        }
-        var outObj = JSON.parse(out);
-        output.textContent = "Iterations: " + outObj.iterations + "\nResult: " + outObj.result;
-        return false;
-    };
-    var resetBtn = document.getElementById('reset');
-    resetBtn.onclick = function (e) {
-        document.getElementById("lurkcode").textContent = originalContents;
-        HighlightLisp.highlight_auto();
-        HighlightLisp.paren_match();
-        var output_container = document.getElementById("output-container");
-        output_container.style.display = "none";
-    }
-});
-*/
